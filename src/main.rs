520f0b2ed4f401e0fe1f69ddde7e2aa1b43632b5
井村龍哉 <imura_tatsuya@applibot.co.jp>
2019-02-21 18:24:58 +0900

    add indexing operations

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     for j in (0..ny).rev() {
         for i in 0..nx {
             let col = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
-            let ir = (255.99*col.r()) as i32;
-            let ig = (255.99*col.g()) as i32;
-            let ib = (255.99*col.b()) as i32;
+            let ir = (255.99*col[0]) as i32;
+            let ig = (255.99*col[1]) as i32;
+            let ib = (255.99*col[2]) as i32;
             println!("{} {} {}",ir,ig,ib);
         }
     }
