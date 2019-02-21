d9c580d7fcc7403bb8da00a190f37f123ff63658
井村龍哉 <imura_tatsuya@applibot.co.jp>
2019-02-22 02:36:11 +0900

    add sphere

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,19 @@ mod ray;
 use vec3::Vec3;
 use ray::Ray;
 
+fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> bool {
+    let oc = r.origin - center;
+    let a = r.direction.dot(r.direction);
+    let b = 2.0 * oc.dot(r.direction);
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4 as f32 *a*c;
+    discriminant > 0.0
+}
+
 fn color(r: Ray) -> Vec3 {
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = Vec3::unit_vector(r.direction);
     let t: f32 = 0.5 * (unit_direction.y() + 1.0);
     (1.0-t)*Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
