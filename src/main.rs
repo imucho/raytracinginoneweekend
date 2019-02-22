39aa24b86d317cd34a72ab5418c309205cff8b6d
imucho <26864686+imucho@users.noreply.github.com>
2019-02-23 02:31:22 +0900

    add antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+extern crate rand;
+
 mod vec3;
 mod ray;
 mod hitable;
 mod hittable_list;
 mod sphere;
+mod camera;
 
 use std::f32;
 use vec3::Vec3;
@@ -10,6 +13,8 @@ use ray::Ray;
 use hitable::*;
 use hittable_list::HitableList;
 use sphere::Sphere;
+use camera::Camera;
+use rand::Rng;
 
 fn color(r: &Ray, world: &Hitable) -> Vec3 {
     if let Some(rec) = world.hit(r, 0.0, f32::MAX) {
@@ -24,20 +29,23 @@ fn color(r: &Ray, world: &Hitable) -> Vec3 {
 fn main() {
     let nx = 200;
     let ny = 100;
+    let ns = 100;
     println!("P3\n{} {}\n255",nx,ny);
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
     let mut world = HitableList::new();
     world.push(Box::new(Sphere {center: Vec3::new(0.0,0.0,-1.0), radius: 0.5}));
     world.push(Box::new(Sphere {center: Vec3::new(0.0,-100.5,-1.0), radius: 100.0}));
+    let mut rng = rand::thread_rng();
+    let cam = Camera::new();
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
-            let col = color(&r, &world);
+            let mut col = Vec3::new(0.0, 0.0, 0.0);
+            for _ in 0..ns {
+                let u = (i as f32 + rng.gen_range(0.0,1.0)) / nx as f32;
+                let v = (j as f32 + rng.gen_range(0.0,1.0)) / ny as f32;
+                let r = cam.get_ray(u, v);
+                col += color(&r, &world);
+            }
+            col /= ns as f32;
             let ir = (255.99*col[0]) as i32;
             let ig = (255.99*col[1]) as i32;
             let ib = (255.99*col[2]) as i32;
